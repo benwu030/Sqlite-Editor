cd379b96e24511bf596208f730debb0cd93ed7e2
benwu <benwu013@connect.hku.hk>
2023-02-02 11:42:31 +0800

    Initial Commit

---
+++ Sqlite Editor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sqlite Editor
+//
+//  Created by Ben WU on 2/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sqlite Editor/Sqlite_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sqlite_EditorApp.swift
+//  Sqlite Editor
+//
+//  Created by Ben WU on 2/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Sqlite_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
